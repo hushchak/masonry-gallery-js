2da80cbbb212dd47f7238721ea91a53bed741be1
Davyd Hushchak <davyd.hushchak@gmail.com>
2026-08-28 20:48:33 +0300

    Add final functionality. Fix mansory-column-style

--- masonry.js
+++ masonry.js
@@ -8,6 +8,43 @@ function arrangeMasonries() {
 function arrangeMasonry(gallery) {
     const columnCount = Math.max(Math.floor(gallery.offsetWidth / 350), 1);
     gallery.style.gridTemplateColumns = `repeat(${columnCount}, 1fr)`;
+
+    const images = getChildMasonryImages(gallery);
+    const columns = [];
+    for (let i = 0; i < columnCount; i++) {
+        columns.push(insertMasonryColumn(gallery));
+    }
+
+    arrangeImagesInColumns(columns, images);
+}
+
+function arrangeImagesInColumns(columns, images) {
+    for (let i = 0; i < images.length; i++) {
+        getShortestElement(columns).appendChild(images[i]);
+    }
+}
+
+function getShortestElement(elements) {
+    let shortestElement = elements[0];
+
+    elements.forEach((element) => {
+        if (element.offsetHeight < shortestElement.offsetHeight) {
+            shortestElement = element;
+        }
+    });
+
+    return shortestElement;
+}
+
+function insertMasonryColumn(element) {
+    const column = document.createElement("div");
+    column.classList.add("masonry-column");
+    element.insertBefore(column, element.firstChild);
+    return column;
+}
+
+function getChildMasonryImages(parentObject) {
+    return parentObject.querySelectorAll(".masonry-image");
 }
 
 arrangeMasonries();
